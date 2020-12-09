9aea9f3e6e8222cfb8a974aba739e0aede2c5a05
Mikeltopkek <leniva9.panda@gmail.com>
2020-12-09 17:02:36 +0200

    add fibonachi.py

---
+++ fibonachi.py
@@ -0,0 +1,11 @@
+def main(n):
+    print(n, end=" ")
+    if n in (1, 2):
+        return 1
+    else:
+        return main(n - 1) + main(n - 2)
+
+
+if __name__ == "__main__":
+    fib = int(input())
+    print(main(fib))
