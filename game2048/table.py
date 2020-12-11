d61b36a15f570d05487523263d84f4f0d54a227e
Mikeltopkek <leniva9.panda@gmail.com>
2020-12-11 20:22:27 +0200

    modify table.py

--- game2048/table.py
+++ game2048/table.py
@@ -9,21 +9,37 @@ def dec1(func):
     return clear
 
 
+"""
+x,y - coordinates of the cell, into which we write a new number
+on start we put a number into a random cell
+"""
+
+
 class Table:
-    def __init__(self, number):
+    def __init__(self, number) -> None:
         self.table = [0]*number
         for i in range(number):
             self.table[i] = [0]*number
         self.number = number
+        self.x = random.randint(0, self.number - 1)
+        self.y = random.randint(0, self.number - 1)
 
     @dec1
-    def display(self):
+    def display(self) -> None:
         for i in range(len(self.table)):
             for j in range(len(self.table[i])):
                 print(self.table[i][j], end=" ")
             print()
 
-    def start(self):
-        self.table[random.randint(0, self.number-1)][random.randint(0, self.number-1)] = 2
+    def start(self) -> None:
+        while self.table[self.x][self.y] != 0:
+            self.x = random.randint(0, self.number - 1)
+            self.y = random.randint(0, self.number - 1)
+        self.table[self.x][self.y] = 2
+
+    @dec1
+    def move_down(self):
+        for i in range(len(self.table)):
+
 
 
